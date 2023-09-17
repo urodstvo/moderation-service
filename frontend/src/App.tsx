15c370e0b22c420ea085117ae96bdec5fc1550bb
CLOWNAKIO <dead.kanji@mail.ru>
2023-09-17 23:46:03 +0300

    feat(routes): Init routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { routes } from "@/pages"
+import Navbar from "./components/Navbar"
+
 function App() {
 
   return (
     <>
-      INIT
+    <Navbar />
+    <main className="main-container">
+        <div className="main-content">
+            <Routes>
+                {routes.map((route, ind) => <Route key={ind} path={route.path} element={route.element}/>)}
+            </Routes>
+        </div>
+    </main>
     </>
   )
 }
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Home from "@/pages/Home";
+import Services from "./Services";
+  
+export const routes = [
+  {
+    path: '/',
+    element: <Home />,
+  },
+  
+  {
+    path: '/services',
+    element: <Services />,
+  },
+]
+
+
