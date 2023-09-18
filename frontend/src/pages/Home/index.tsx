d93126ff4307f7b855a8ec8ef56bf95ca41de338
CLOWNAKIO <dead.kanji@mail.ru>
2023-09-18 20:06:22 +0300

    feat: adapted for service card

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import styles from "@/pages/Home/index.module.css"
 
-import { variants } from "@/interfaces";
+import { ColorVariant } from "@/interfaces";
 import { Link } from "react-router-dom";
 import Button from "@/components/ui/Button"
 
@@ -9,7 +9,7 @@ const Home = () => {
 
     return (
         <>
-        <div className={styles.heroContainer}>
+        <header className={styles.heroContainer}>
             <div className={styles.heroContent}>
                 <div className={styles.heroTitle}>
                     WELCOME TO THE <span>CLOUD</span> PLATFORM
@@ -20,15 +20,17 @@ const Home = () => {
                 </div>
 
                 <div className={styles.heroActions}>
-                    <Button text='GET STARTED' variant={variants.black}/>
-                    <Link to='/services'><Button text='CHECK SERVICES' variant={variants.white} /></Link>
+                    <Button text='GET STARTED' variant={ColorVariant.black}/>
+                    <Link to='/services'><Button text='CHECK SERVICES' variant={ColorVariant.white} /></Link>
                 </div>
                 
                 <div className={styles.heroImage} />
             </div>
-        </div>
+        </header>
         </>
     );
 };
 
 export default Home;
+
+//TODO: ADD RESPONSIVE TO HERO
