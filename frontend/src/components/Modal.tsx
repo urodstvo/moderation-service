c8d8f9661e5ef7811f001736fe6071a38e64584a
CLOWNAKIO <dead.kanji@mail.ru>
2023-09-21 16:22:17 +0300

    feat: added Modal closing handler

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -1,10 +1,16 @@
-import { ReactNode } from "react";
+import { MouseEventHandler, ReactNode, useRef } from "react";
 
 const Modal = ({children} : {children: ReactNode}) => {
+    const container = useRef(null);
+    const close = useRef(null);
+
+    const back : MouseEventHandler = (e) => {
+        if (e.target === container.current || (e.target as SVGAElement).closest('.modal-close') === close.current)
+                history.back();
+    }
     return (
-        <div className="modal-container">
-            <div className="modal-close"
-            onClick={() => history.back()}>
+        <div className="modal-container" ref={container} onClick={back}>
+            <div className="modal-close" ref={close} onClick={back}>
                 <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
             </div>
             <div className="modal-content">
