fa68c8447e5f8664e61a126110772820a459d57d
CLOWNAKIO <dead.kanji@mail.ru>
2023-09-29 12:03:32 +0300

    feat(UserManager): added get user func

--- backend/src/auth/manager.py
+++ backend/src/auth/manager.py
@@ -1,20 +1,44 @@
-from src.auth.models import User, ShowUser, CreateUser
+from typing import Union
+
+from sqlalchemy import select, or_, and_
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.auth.models import User, GetUserResponse, SignUpRequest, SignInRequest
 
 
 class UserManager:
     """Class for interacting with User Model"""
 
     @staticmethod
-    async def createUser(data: CreateUser, db) -> ShowUser:
+    async def createUser(data: SignUpRequest, db) -> GetUserResponse:
         async with db as session:
             async with session.begin():
                 user = User(username=data.username, password=data.password, email=data.email)
                 session.add(user)
                 await session.flush()
-                return ShowUser(
+                return GetUserResponse(
                     user_id=user.user_id,
                     username=user.username,
                     email=user.email,
                     is_verified=user.is_verified,
                     registered_at=user.registered_at
                 )
+
+    @staticmethod
+    async def getUser(data: SignInRequest, db: AsyncSession) -> Union[GetUserResponse, None]:
+        async with db as session:
+            async with session.begin():
+                query = select(User).where(and_(or_(User.username == data.login,
+                                                    User.email == data.login),
+                                                User.password == data.password))
+                res = await db.execute(query)
+                user_row = res.fetchone()
+                if user_row is not None:
+                    user = user_row[0]
+                    return GetUserResponse(
+                        user_id=user.user_id,
+                        username=user.username,
+                        email=user.email,
+                        is_verified=user.is_verified,
+                        registered_at=user.registered_at
+                    )
