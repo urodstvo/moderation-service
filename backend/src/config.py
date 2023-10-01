0e780e99b69f0de80182b4c4c94d5b9f64c51f41
CLOWNAKIO <dead.kanji@mail.ru>
2023-10-01 20:01:04 +0300

    feat: added Email config

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,8 +1,21 @@
 import os
 
 from dotenv import load_dotenv, find_dotenv
+from fastapi_mail import ConnectionConfig
 
 load_dotenv(find_dotenv())
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 JWT_SECRET = os.getenv("JWT_SECRET")
+
+EmailConfig = ConnectionConfig(
+    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
+    MAIL_FROM=os.getenv("MAIL_FROM"),
+    MAIL_PORT=int(os.getenv("MAIL_PORT")),
+    MAIL_SERVER=os.getenv("MAIL_SERVER"),
+    MAIL_STARTTLS=False,
+    MAIL_SSL_TLS=True,
+    USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True
+)
