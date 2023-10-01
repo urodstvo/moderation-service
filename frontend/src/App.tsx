f2305d7cc1af34f1eaf05235f3eaa611c2fe6be2
CLOWNAKIO <dead.kanji@mail.ru>
2023-10-01 16:29:41 +0300

    feat: changed modal handler

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,27 +4,28 @@ import Navbar from '@/components/Navbar.tsx'
 import Modal from './components/Modal';
 import SignInForm from './components/SignInForm';
 import { useEffect } from 'react';
-import { useAppDispatch } from './hooks';
+import { useAppDispatch, useAppSelector } from './hooks';
 import { fetchDataFromStorage } from './store/auth';
 
 const App = () => {
     const [searchParams, SetSearchParams] = useSearchParams();
-    const modal = searchParams.get('modal');
     const dispatch = useAppDispatch()
+    const { isAuth } = useAppSelector(state => state.auth)
 
     useEffect(() => {
         dispatch(fetchDataFromStorage());
     }, [])
 
+    const signInModal = () => {
+        if (!!searchParams.get('modal'))
+            if (!isAuth)
+                return (<Modal><SignInForm /></Modal>)
+            else SetSearchParams(prev => prev.toString().replace("modal=signIn", ''));
+    }
+
     return (
     <>
-    {modal && (
-        <Modal>
-            {modal === "signIn" && (
-                <SignInForm />
-            )}
-        </Modal>
-    )}
+    {signInModal()}
     <Navbar />
     <main className="main-container">
         <div className="main-content">
