2dd12b82bee2547d1caf5f9100233cdce8aa146c
CLOWNAKIO <dead.kanji@mail.ru>
2023-10-01 15:18:06 +0300

    feat(redux): init redux store

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,12 +3,16 @@ import '@/index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App'
+import App from '@/App'
+import { Provider } from 'react-redux'
+import { store } from '@/store'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Provider store={store}>
       <BrowserRouter basename='/'>
         <App />
       </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
