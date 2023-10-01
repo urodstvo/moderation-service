11343a480efba1bc59025c809444ae84f97a2261
CLOWNAKIO <dead.kanji@mail.ru>
2023-10-01 15:16:48 +0300

    feat(server): add dev middlewares

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from src.auth.router import auth_router
 
 
 app = FastAPI(title="MODERATION SERVICE")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(
     auth_router,
