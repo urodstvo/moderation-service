9672dcd3db02e26da9580cb30861b1dec8014072
CLOWNAKIO <dead.kanji@mail.ru>
2023-10-03 10:06:03 +0300

    feat(Timer): added Timer component

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,7 +1,23 @@
 import { useDispatch, useSelector } from 'react-redux'
 import type { TypedUseSelectorHook } from 'react-redux'
 import type { RootState, AppDispatch } from '@/store'
+import { useEffect, useState } from 'react'
 
 
 export const useAppDispatch: () => AppDispatch = useDispatch
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+
+export const useCountDown = (seconds : number) => {
+    const [countDown, setCountDown] = useState(seconds);
+  
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setCountDown(prev => prev - 1);
+      }, 1000);
+  
+      return () => clearInterval(interval);
+    }, []);
+  
+    return countDown;
+}
+
