6463600e868d153b48d1461a1130cd7f7a0f9abc
CLOWNAKIO <dead.kanji@mail.ru>
2023-10-12 14:44:49 +0300

    refactor: changed to generic methods

--- backend/src/manager.py
+++ backend/src/manager.py
@@ -4,72 +4,41 @@ from typing import Union
 from sqlalchemy import select, or_, and_, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.models import User, GetUserResponse, SignUpRequest, SignInRequest, ModerationData, TextModeration
+from src.models import User, UserResponse, SignUpRequest, SignInRequest, ModerationData, TextModeration, GetUser, \
+    UpdateUser
 
 
 class UserManager:
     """Class for interacting with User Model"""
 
     @staticmethod
-    async def createUser(data: SignUpRequest, db: AsyncSession) -> GetUserResponse:
+    async def createUser(data: SignUpRequest, db: AsyncSession) -> UserResponse:
         async with db as session:
             async with session.begin():
                 user = User(username=data.username, password=data.password, email=data.email)
                 session.add(user)
                 await session.flush()
-                return GetUserResponse(
-                    user_id=user.user_id,
-                    username=user.username,
-                    email=user.email,
-                    is_verified=user.is_verified,
-                    registered_at=user.registered_at,
-                    role=user.role
-                )
+                return UserResponse(**user.as_dict())
 
     @staticmethod
-    async def getUser(data: SignInRequest, db: AsyncSession) -> Union[GetUserResponse, None]:
+    async def getUser(user_data: dict, db: AsyncSession) -> Union[UserResponse, None]:
         async with db as session:
             async with session.begin():
-                query = select(User).where(and_(or_(User.username == data.login,
-                                                    User.email == data.login),
-                                                User.password == data.password))
-                res = await db.execute(query)
-                user_row = res.fetchone()
-                if user_row is not None:
-                    user = user_row[0]
-                    return GetUserResponse(
-                        user_id=user.user_id,
-                        username=user.username,
-                        email=user.email,
-                        is_verified=user.is_verified,
-                        registered_at=user.registered_at,
-                        role=user.role
-                    )
+                user = await session.execute(select(User).filter_by(**user_data))
+                user = user.first()[0]
+                return None if user is None else UserResponse(**user.as_dict())
 
     @staticmethod
-    async def getUserByUsername(username: str, db: AsyncSession) -> Union[User, None]:
+    async def updateUser(user_data: dict, update_data: dict, db: AsyncSession) -> None:
         async with db as session:
             async with session.begin():
-                query = select(User).where(User.username == username)
-                res = await db.execute(query)
-                user_row = res.fetchone()
-                if user_row is not None:
-                    user = user_row[0]
-                    return GetUserResponse(
-                        user_id=user.user_id,
-                        username=user.username,
-                        email=user.email,
-                        is_verified=user.is_verified,
-                        registered_at=user.registered_at,
-                        role=user.role
-                    )
+                user = await session.execute(select(User).filter_by(**user_data))
+                user = user.first()
+                if user is None:
+                    raise Exception("User doesn't exist")
 
-    @staticmethod
-    async def verifyUser(user_id: uuid.UUID, db: AsyncSession) -> None:
-        async with db as session:
-            async with session.begin():
-                query = update(User).where(User.id == user_id).values(is_verified=True)
-                await db.execute(query)
+                user.update(**update_data)
+                await db.commit()
 
 
 class ModerationManager:
@@ -82,12 +51,9 @@ class ModerationManager:
                 request = data.table(user_id=data.user_id)
                 if "text" in kwargs:
                     request.text = kwargs['text']
-                # request = TextModeration(user_id=data.user_id, text=data.text)
 
                 session.add(request)
                 await session.flush()
-
-                # TODO: check if working that
 
     @staticmethod
     async def getCount(data: ModerationData, db: AsyncSession, **kwargs) -> int:
