4674e0acd65213dfb109d0382ea11ad2d34ea0fe
CLOWNAKIO <dead.kanji@mail.ru>
2023-10-13 12:31:32 +0300

    feat: added pricing plan changer

--- frontend/src/pages/Pricing/components/PlanCard/PlanCard.tsx
+++ frontend/src/pages/Pricing/components/PlanCard/PlanCard.tsx
@@ -3,8 +3,9 @@ import { ColorVariant, iPricingCardProps } from "@/interfaces";
 import Button from "@/components/ui/Button";
 import { useAppSelector } from "@/hooks";
 
-export const PlanCard = ({name, price, offers, terms } : iPricingCardProps) => {
-    const {user} = useAppSelector(state => state.auth)
+export const PlanCard = ({name, price, offers, terms, disabled = false, onChoose } : iPricingCardProps) => {
+    const { user } = useAppSelector(state => state.auth)
+    const choosen = name.toLowerCase() === user?.role
     return (
         <div className={styles.pricingCardContainer}>
             <div className={styles.pricingCardContent}>
@@ -20,7 +21,13 @@ export const PlanCard = ({name, price, offers, terms } : iPricingCardProps) => {
                     {terms.map((text, ind) => <li key={ind} className={styles.pricingCardTerm}>{text}</li>)}
                 </ul>
                 <div className={styles.pricingCardButton}>
-                    <Button className="fill-container" text="CHOOSE PLAN" variant={ColorVariant.white} disabled={!user?.is_verified}/>
+                    <Button 
+                        className="fill-container" 
+                        text={choosen ? "CHOOSEN" : "CHOOSE PLAN"}
+                        variant={choosen ? ColorVariant.black : ColorVariant.white} 
+                        disabled={disabled}
+                        onClick={onChoose}
+                    />
                 </div>
             </div>
         </div>
--- frontend/src/pages/Pricing/index.tsx
+++ frontend/src/pages/Pricing/index.tsx
@@ -1,15 +1,52 @@
+import { useAppDispatch, useAppSelector, usePageTitle } from "@/hooks";
+import { RoleEnum } from "@/interfaces";
 import { PlanCard } from "@/pages/Pricing/components/PlanCard/PlanCard";
 import styles from "@/pages/Pricing/index.module.css"
+import { setRole } from "@/store/auth";
 
 export const Pricing = () => {
+    usePageTitle("PRICING PLAN | CLOUD")
+    const { token, user } = useAppSelector(state => state.auth)
+    const dispatch = useAppDispatch()
+    const student_disabled = !user?.is_verified || user.role !== RoleEnum.User
+    
+    const chooseStudent = () => {
+        if (token)
+            (async () => {
+                const response = await fetch("http://localhost:8000/api/role/student",{
+                    method: "PATCH",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Accept: "application/json",
+                        Authorization: token
+                    },
+                })
+
+                if (response.ok) dispatch(setRole({role: RoleEnum.Student}))
+            })()
+    }
+
     return (
         <>
         <h1 className="text-center">PRICING</h1>
         <section>
             <div className={styles.plansContainer}>
                 <div className={styles.plansContent}>
-                    <PlanCard name="STUDENT" price="FREE" offers={["1000 requests per day", "Text Moderation service", "Image Moderation service"]} terms={["Verified Email"]}/>
-                    <PlanCard name="COMPANY" price="FREE" offers={["Unlimited requests per day", "Text Moderation service", "Image Moderation service", "Audio Moderation Service", "Video Moderation service"]} terms={["Verified Email", "Admin Approval"]}/>
+                    <PlanCard 
+                        name="STUDENT" 
+                        price="FREE" 
+                        offers={["1000 requests per day", "Text Moderation service", "Image Moderation service"]} 
+                        terms={["Verified Email"]}
+                        disabled={student_disabled}
+                        onChoose={chooseStudent}
+                    />
+                    <PlanCard 
+                        name="COMPANY" 
+                        price="FREE" 
+                        offers={["Unlimited requests per day", "Text Moderation service", "Image Moderation service", "Audio Moderation Service", "Video Moderation service"]} 
+                        terms={["Verified Email", "Admin Approval"]}
+                        disabled
+                    />
                 </div>
             </div>
         </section>
