d89b47e7367e8c3faf71df3488aa44dc7b3ad161
CLOWNAKIO <dead.kanji@mail.ru>
2024-04-23 20:10:09 +0300

    chore: updated NotFound page

--- frontend/src/app/[locale]/not-found.tsx
+++ frontend/src/app/[locale]/not-found.tsx
@@ -1,3 +1,15 @@
+import { Button } from '@/components/ui/button';
+import { Link } from '@/navigation';
+
 export default function NotFoundPage() {
-    return <h1>Oops... sorry</h1>;
+    return (
+        <main className='flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#222222] to-[#000000]'>
+            <h1 className='font-overpass text-[100px] font-black text-white'>Page Not Found</h1>
+            <Button asChild className='py-5 w-[300px] bg-transparent' size={null} variant='outline'>
+                <Link href='/' className='font-roboto text-xl font-bold text-white'>
+                    GO HOME
+                </Link>
+            </Button>
+        </main>
+    );
 }
