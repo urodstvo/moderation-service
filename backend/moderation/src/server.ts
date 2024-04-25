57a7df1d580741c677d8ba6e264eef18e352c823
CLOWNAKIO <dead.kanji@mail.ru>
2024-04-25 11:30:43 +0300

    build: configured moderation app

--- backend/moderation/src/server.ts
+++ backend/moderation/src/server.ts
@@ -1,15 +1,12 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
-
-dotenv.config();
+import express, { Express, Response } from "express";
 
 const app: Express = express();
-const port = process.env.PORT || 8000;
+const port: number = 8000;
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello world!");
+app.get("/", (_, res: Response) => {
+    res.status(200).send("Hello World!");
 });
 
 app.listen(port, () => {
-    console.log(`[server]: Server is running at port = ${port}`);
+    console.log(`[server]: Server is running at http://localhost:${port}`);
 });
