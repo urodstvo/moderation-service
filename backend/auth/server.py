6b93d2c5172703fa9a01692d63ce2ee544d7f4a7
CLOWNAKIO <dead.kanji@mail.ru>
2024-05-02 22:47:23 +0300

    chore: creating admin user

--- backend/auth/server.py
+++ backend/auth/server.py
@@ -2,16 +2,25 @@ import uvicorn
 from fastapi import FastAPI, Depends
 from starlette.middleware.cors import CORSMiddleware
 
-
 from src.config import redis
-from src.db.base import init_db
+
+from src.db.base import init_db, AsyncSession
+
 from src.handlers.api import api_router
 from src.handlers.auth import auth_router
 from src.handlers.email import email_router
 from src.handlers.password import password_router
 from src.handlers.profile import profile_router
 from src.handlers.admin import admin_router
-from src.util import JWTBearer
+
+from src.util import JWTBearer, hash_password
+
+from src.DTO.auth import CreateUserData
+
+from src.db.tables.users import UsersTable
+from src.db.tables.profiles import ProfilesTable
+
+from src.db.models import ProfileModel, UserModel
 
 app = FastAPI(title="SERVICE", root_path="/api")
 
@@ -66,6 +75,8 @@ app.include_router(
 async def on_startup():
     await init_db()
 
+    
+
 
 @app.on_event("shutdown")
 async def on_shutdown():
@@ -73,8 +84,19 @@ async def on_shutdown():
 
 
 @app.get("/")
-async def ping():
-    return "hello world"
+async def ping(db: AsyncSession):
+    user_data = CreateUserData(
+        email='admin@admin.com',
+        password=hash_password('admin'),
+    )
+    try:
+        user_id = await UsersTable.createUser(user_data, db)
+        await UsersTable.updateUser(UserModel(user_id=user_id), UserModel(is_verified=True), db)
+        await ProfilesTable.updateProfile(ProfileModel(user_id=user_id), ProfileModel(role='admin'), db)
+    except: 
+        return 'admin is already exists'
+
+    return "admin is created"
 
 
 if __name__ == "__main__":
